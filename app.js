b7b305ba5ba5352530ea21930052091b99597511
Maggie <mengjing0818@gmail.com>
2020-12-04 00:42:38 -0800

    Styling show page

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
 });
 
 const db = mongoose.connection;
