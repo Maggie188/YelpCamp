6e9c01c60cad0f35885932bc3af8479614a95ca2
Maggie <mengjing0818@gmail.com>
2020-12-17 00:21:41 -0800

    Login routes.

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
-})
+});
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views/'));
 
@@ -47,7 +47,8 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
+};
+
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
@@ -61,7 +62,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
@@ -70,21 +71,21 @@ app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 
 
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
-})
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Somrthing Went Wrong!'
     res.status(statusCode).render('error', {err});
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
-})
+});
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 
+
 router.get('/register', (req, res)=> {
     res.render('users/register');
-})
+});
 
 router.post('/register', catchAsync (async (req, res) => {
     try {
@@ -17,9 +19,18 @@ router.post('/register', catchAsync (async (req, res) => {
         res.redirect('/campgrounds');
     } catch (e) {
         req.flash('error', e.message);
-        res.redirect('register');
+        res.redirect('/register');
     }
     
-}))
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
