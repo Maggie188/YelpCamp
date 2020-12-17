796b53dcaf76ee357de331e92a025dee84721455
Maggie <mengjing0818@gmail.com>
2020-12-16 21:26:29 -0800

    Register form.

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -62,8 +63,10 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
+
 
 app.get('/', (req, res) => {
     res.render('home');
