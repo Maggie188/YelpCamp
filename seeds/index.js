8ce5b2e71e834295d0479b4f140b11dc80f7cfdd
Maggie <mengjing0818@gmail.com>
2020-12-17 22:58:41 -0800

    Added an author to campground.

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5fdb14ac48a7b26b89fe3fe8',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
