2dd7a8193ea6ba4abc27bbe54d98bcf95b5590ac
Maggie <mengjing0818@gmail.com>
2020-12-17 23:54:40 -0800

    Authorization middleware for Campground.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError =  require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const {campgroundSchema} = require('../schemas.js');
-const { isLoggedIn } = require('../middleware');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
-const validateCampground = (req, res, next) => {
-    const {error} = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();    
-    }
-};
+
+
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -39,31 +30,22 @@ router.get('/:id', catchAsync(async (req, res) => {
     if (!campground) {
         req.flash('error', 'Cannot find that campground!')
         return res.redirect('/campgrounds');
-    }
+    };
     res.render('campgrounds/show', { campground });
 }));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!')
         return res.redirct('/campgrounds');
     };
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
-    };
     res.render('campgrounds/edit', { campground });
 }));
 
-router.put('/:id', isLoggedIn, validateCampground, async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
-    }
     const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const { validateReview } = require('../middleware');
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
@@ -7,17 +8,7 @@ const Review = require('../models/review');
 const ExpressError =  require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
-const {reviewSchema} = require('../schemas.js');
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();    
-    }
-}
 
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
