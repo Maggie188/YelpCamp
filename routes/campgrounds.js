b5bee95368d20d497ad5ee5ebfca62353e0cf7cc
Maggie <mengjing0818@gmail.com>
2020-12-19 18:22:51 -0800

    Restructured Routes.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,21 +8,17 @@ const Campground = require('../models/campground');
 const campground = require('../models/campground');
 
 
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, campgrounds.updateCampground)
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, campgrounds.updateCampground);
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
-
-
 
 module.exports = router;
